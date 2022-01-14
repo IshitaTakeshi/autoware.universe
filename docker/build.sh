8d3c8e47ec0a22abbbe9a5a34321b9c89a93e9ec
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-01-14 21:09:36 +0900

    Resolve dependencies based on the host's src

--- docker/build.sh
+++ docker/build.sh
@@ -8,6 +8,12 @@ WORKSPACE_ROOT="$SCRIPT_DIR/../"
 # https://github.com/docker/buildx/issues/484
 export BUILDKIT_STEP_LOG_MAX_SIZE=10000000
 
+if [ ! -d "src" ]
+then
+    mkdir src
+    vcs import src < autoware.repos
+fi
+
 docker buildx bake --progress=plain -f "$SCRIPT_DIR/autoware-universe/docker-bake.hcl" \
     --set "*.context=$WORKSPACE_ROOT" \
     --set "*.ssh=default" \
