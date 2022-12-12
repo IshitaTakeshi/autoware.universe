0eb78aef83339ff8aba07a739687ca41cd73a14f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-12-12 22:19:43 +0900

    Move functions in edge.hpp to edge.cpp

--- localization/lidar_feature_localization/src/localization/edge.cpp
+++ localization/lidar_feature_localization/src/localization/edge.cpp
@@ -84,3 +84,54 @@ Eigen::Vector3d MakeEdgeResidual(
   const Eigen::Vector3d p = transform * p0;
   return (p - p1).cross(p - p2);
 }
+
+Edge::Edge(const pcl::PointCloud<pcl::PointXYZ>::Ptr & edge_map, const size_t n_neighbors)
+: kdtree_(edge_map), n_neighbors_(n_neighbors)
+{
+}
+
+std::tuple<std::vector<Eigen::MatrixXd>, std::vector<Eigen::VectorXd>> Edge::Make(
+  const pcl::PointCloud<pcl::PointXYZ>::Ptr & scan,
+  const Eigen::Isometry3d & point_to_map) const
+{
+  // f(dx) \approx f(0) + J * dx + dx^T * H * dx
+  // dx can be obtained by solving H * dx = -J
+
+  const size_t n = scan->size();
+
+  const Eigen::Quaterniond q(point_to_map.rotation());
+
+  const auto transformed = TransformPointCloud<pcl::PointXYZ>(point_to_map, scan);
+
+  std::vector<Eigen::MatrixXd> jacobians;
+  std::vector<Eigen::VectorXd> residuals;
+
+  for (size_t i = 0; i < n; i++) {
+    const pcl::PointXYZ query = transformed->at(i);
+    const auto [mean, covariance] = this->NeighborMeanAndCovariance(query);
+    const Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> ed = EigenDecomposition(covariance);
+    const Eigen::Vector3d eigenvalues = ed.eigenvalues();
+
+    if (IsEdge(eigenvalues)) {
+      continue;
+    }
+
+    const Eigen::Vector3d principal = ed.eigenvectors().col(2);
+    const Eigen::Vector3d p0 = PointXYZVectorConversion::PointToVector(scan->at(i));
+    const Eigen::Vector3d p1 = mean - principal;
+    const Eigen::Vector3d p2 = mean + principal;
+
+    jacobians.push_back(MakeEdgeJacobianRow(q, p0, p1, p2));
+    residuals.push_back(MakeEdgeResidual(point_to_map, p0, p1, p2));
+  }
+
+  return std::make_tuple(jacobians, residuals);
+}
+
+std::tuple<Eigen::VectorXd, Eigen::MatrixXd> Edge::NeighborMeanAndCovariance(
+  const pcl::PointXYZ & query) const
+{
+  const pcl::PointCloud<pcl::PointXYZ> neighbors = kdtree_.NearestKSearch(query, n_neighbors_);
+  const Eigen::MatrixXd X = GetXYZ(neighbors);
+  return CalcMeanAndCovariance(X);
+}
